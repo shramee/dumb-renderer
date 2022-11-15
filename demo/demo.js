37452e75ff702f6083aa2e726296a5349e8b40cc
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-11-15 17:07:43 +0000

    Add image sizes and transforms

--- demo/demo.js
+++ demo/demo.js
@@ -9,34 +9,59 @@
 
   const assets = {
     user: "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/user.svg",
-    asterisk:
-      "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/asterisk.svg",
-    times:
-      "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/times.svg",
+    asterisk: "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/asterisk.svg",
+    times: "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/times.svg",
   };
 
   const assetsArr = [
     {
       id: "circle",
       src: "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/circle-o.svg",
+      w: 50,
+      h: 50,
       states: {
         dot: "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/dot-circle-o.svg",
         squish: {
-          transform: "1.00,0.00,0.00,0.10,0,0",
+          transform: [1.0, 0.0, 0.0, 0.1, 0, 0],
         },
       },
     },
   ];
-  for (asset in assets) {
+  for (const asset in assets) {
     assetsArr.push({
       id: asset,
       src: assets[asset],
+      w: 70,
+      h: 70,
     });
   }
-  console.log(assetsArr);
 
   // Wait for assets to load
   await renderer.loadAssets(assetsArr);
 
-  console.log(renderer.assets);
+  // Can be called to render again after changes
+  renderer.render([
+    {
+      id: "asterisk",
+      x: 200,
+      y: 100,
+    },
+    {
+      id: "circle",
+      state: "dot",
+      x: 300,
+      y: 200,
+    },
+    {
+      id: "circle",
+      state: "squish",
+      x: 500,
+      y: 100,
+    },
+    {
+      id: "user",
+      x: 500,
+      y: 250,
+    },
+  ]);
 })(window.DumbRenderer);
