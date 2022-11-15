693e20c647e4a03ff139311d627922de6316c3d5
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-11-15 08:53:20 +0000

    Demo with parcel

---
+++ demo/demo.js
@@ -0,0 +1,6 @@
+const renderer = new window.DumbRenderer();
+
+renderer.setupCanvas({
+  w: 800,
+  h: 450,
+});
--- index.ts
+++ index.ts
@@ -14,7 +14,6 @@ interface CanvasProps {
   h: number;
   w: number;
 }
-
 interface DumbRendererConstructor {
   new (): DumbRenderer;
 }
